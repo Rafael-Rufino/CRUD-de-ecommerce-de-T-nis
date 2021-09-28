3b64feb6777a5416fec42d6ba7d0e523fb3e5661
RafaelRufino <rafaelrufino.52@gmail.com>
2021-09-28 14:27:14 -0300

    adicionando o api e redux

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'prettier', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 0,
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 0,
+    'import/newline-after-import': 0,
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+};
